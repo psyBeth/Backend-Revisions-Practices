e57e7f37c26e45a02a8e46324b06587685a92707
Betul <betulkoru06@gmail.com>
2024-04-06 02:47:10 +0300

    blog post model built

--- 09-blogAPI-2/src/models/blog.js
+++ 09-blogAPI-2/src/models/blog.js
@@ -18,7 +18,34 @@ const blogCategorySchema = new mongoose.Schema({
 
 //* BLOG POST
 
+const blogPostSchema = new mongoose.Schema({
+    // _id
+    categoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'blogCategory',
+        required: true
+    },
+    title: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    content: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    published: {
+        type: Boolean,
+        default: true
+    }
+} , {
+    collection: 'blogPost',
+    timestamps: true
+});
+
 
 module.exports = {
-    blogCategory: mongoose.model('blogCategory', blogCategorySchema)
-}
+    blogCategory: mongoose.model('blogCategory', blogCategorySchema),
+    blogPost: mongoose.model('blogPost', blogPostSchema)
+};
