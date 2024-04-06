ca8b23a314fc40ec8d568815466b870f95531430
Betul <betulkoru06@gmail.com>
2024-04-06 03:04:12 +0300

    blog post controller built

--- 09-blogAPI-2/src/controllers/blog.js
+++ 09-blogAPI-2/src/controllers/blog.js
@@ -2,7 +2,7 @@
 
 require('express-async-errors');
 
-const { blogCategory } = require('../models/blog');
+const { blogCategory, blogPost } = require('../models/blog');
 
 module.exports.blogCategory = {
     list: async(req, res) => {
@@ -45,6 +45,53 @@ module.exports.blogCategory = {
     delete: async(req, res) => {
         const data = await blogCategory.deleteOne({_id: req.params.categoryId});
         // console.log(data);
-        res.sendStatus((data.deletedCount>=1)? 204:404);
+        res.sendStatus((data.deletedCount>=1)? 204 : 404);
+    }
+};
+
+module.exports.blogPost = {
+    //TODO: filtering, searching, sorting, pagination will be here -- or in a seperate file
+    //! DON'T FORGETTTT!!!!
+
+    list: async(req, res) => {
+        const data = await res.getModelList(blogPost, 'blogCategoryId');
+        res.status(200).send({
+            error: false,
+            details: await res.getModelList(blogPost),
+            data: data
+        });
+    },
+
+    create: async(req, res) => {
+        const data = await blogPost.create(req.body);
+        res.status(201).send({
+            error: false,
+            body: req.body,
+            data: data
+        });
+    },
+
+    read: async(req, res) => {
+        const data = await blogPost.find({_id: req.params.postId});
+        res.status(202).send({
+            error: false,
+            data: data
+        });
+    },
+
+    update: async(req, res) => {
+        const data = await blogPost.updateOne({_id: req.params.postId}, req.body);
+        const newdata = await blogPost.find({_id: req.params.postId});
+        res.status(203).send({
+            error: false,
+            body: req.body,
+            data: data,
+            newdata: newdata
+        })
+    },
+
+    delete: async(req, res) => {
+        const data = await blogPost.deleteOne({_id: req.param.postId});
+        res.sendStatus((data.deletedCount >= 1)? 204 : 404)
     }
 };
