b4a7f05fd249ceb5936edea113d326e8c27542ea
Betul <betulkoru06@gmail.com>
2024-04-08 02:35:14 +0300

    user control midlleware

--- 09-blogAPI-2/index.js
+++ 09-blogAPI-2/index.js
@@ -7,7 +7,7 @@
     npm i cookie-session
 */
 
-const express= require('express');
+const express = require('express');
 const app = express();
 
 app.use(express.json());
@@ -18,19 +18,37 @@ const HOST = process.env.HOST;
 
 require('./src/configs/dbConnection');
 
-// MIDDLEWARES
+//? MIDDLEWARES
+app.use(require('./src/middlewares/userControl'));
 app.use(require('./src/middlewares/findSearchSortPage'));
+
+app.all('/', (req, res) => {
+    if (req.isLogin) {
+        res.send({
+            error: false,
+            message: 'WELLCOME BLOG API PROJECT',
+            session: req.session,
+            user: req.user
+        })
+    } else {
+        res.send({
+            error: false,
+            message: 'WELLCOME BLOG API PROJECT',
+            session: req.session,
+        })
+    };
+});
 
 app.all('/', (req, res) => {
     res.send('wellcome, app is working')
 });
 
 
-// ROUTES
+//? ROUTES
 app.use('/blog', require('./src/routes/blog'));
 app.use('/user', require('./src/routes/user'));
 
-// ERROR HANDLER
+//? ERROR HANDLER
 app.use(require('./src/middlewares/errorHandler'));
 
 app.listen(PORT, () => console.log(`Server is running on -> http://${HOST}:${PORT}`));
