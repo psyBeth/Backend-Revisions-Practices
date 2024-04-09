3c79544003385f36731290643b0ca6a6f9ba2154
Betul <betulkoru06@gmail.com>
2024-04-09 03:30:57 +0300

    index intro db connect

--- 10-personnelAPI-2/index.js
+++ 10-personnelAPI-2/index.js
@@ -5,3 +5,27 @@
     npm i cookie-session
     npm i jsonwebtoken
 */
+
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+const HOST = process.env?.HOST || 8000;
+const PORT = process.env.PORT;
+
+require('express-async-errors');
+
+// CONFIGURATION DATABASE:
+const { dbConnection } = require('./src/configs/dbConnection');
+dbConnection();
+
+// MIDDLEWARES:
+
+
+// ROUTES:
+
+
+// ERROR HANDLER:
+
+
+//
