a3ca9e97e75ecfba8453ffdd9a2f04ffafd7013c
Betul <betulkoru06@gmail.com>
2024-04-10 22:07:13 +0300

    password encrypt helper

---
+++ 10-personnelAPI-2/src/helpers/passwordEncrypt.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const crypto = require('node:crypto');
+const keyCode = process.env.SECRET_KEY || 'pls_write_random_chars_here';
+const loopCount = 10_000;
+const charCount = 32;
+const encType = 'sha512';
+
+module.exports = function(password) {
+    return crypto.pbkdf2Sync(password, keyCode, loopCount, charCount, encType).toString('hex')
+};
