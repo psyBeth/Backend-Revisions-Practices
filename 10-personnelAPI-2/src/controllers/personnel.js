7012df95e273e9c91e6acec5b94c231d82715779
Betul <betulkoru06@gmail.com>
2024-04-11 22:48:44 +0300

    login logout personnel cntrlr

--- 10-personnelAPI-2/src/controllers/personnel.js
+++ 10-personnelAPI-2/src/controllers/personnel.js
@@ -61,5 +61,51 @@ module.exports = {
         });
     },
 
-    //TODO: login logout will be here
+    //? LOGIN & LOGOUT
+
+    login: async(req, res) => {
+        const { username, password } = req.body;
+
+        if(username && password){
+
+            const user = await Personnel.findOne({ username, password})
+            
+            if(user) {
+                // set session
+                req.session = {
+                    id: user._id,
+                    password: user.password
+                };
+
+                // set cookie
+                if(req.body?.rememberMe){
+                    req.sessionOptions.maxAge = 1000 * 60 * 60 * 24 * 3 // 3 days
+                }
+
+                res.status(200).send({
+                    error: false,
+                    user
+                });
+
+            } else {
+                res.errorStatusCode = 401;
+                throw new Error('Wrong username or password.');
+            }
+
+        } else {
+            res.errorStatusCode = 401;
+            throw new Error('Please enter username and password.')
+        }
+
+    },
+
+
+    logout: async(req, res) => {
+        // set session to null:
+        req.session = null;
+        res.status(200).send({
+            error: false,
+            message: 'LOGOUT: sessions deleted'
+        });
+    }
 };
