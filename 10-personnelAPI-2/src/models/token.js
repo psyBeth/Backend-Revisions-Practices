525ca2864366e57c65c9eb110589201b3a0879a8
Betul <betulkoru06@gmail.com>
2024-04-12 23:54:50 +0300

    token model

---
+++ 10-personnelAPI-2/src/models/token.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const { mongoose } = require('../configs/dbConnection');
+
+/* ------------------------------------------------------- *
+{
+    "userId": "exampleUserId",
+    "token": "...tokenKey..."
+}
+/* ------------------------------------------------------- */
+
+const TokenSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Personnel',
+        required: true,
+        index: true,
+        unique: true
+    },
+
+    token: {
+        type: String,
+        trim: true,
+        required: true,
+        index: true,
+        unique: true,
+    }
+
+} , {
+    collection: 'tokens',
+    timestamps: true
+});
+
+module.exports = mongoose.model('Token', TokenSchema);
