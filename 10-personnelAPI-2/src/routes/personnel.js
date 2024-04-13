3f523b8fbab706809cbfc17e6444b93325be55df
Betul <betulkoru06@gmail.com>
2024-04-13 22:37:43 +0300

    permissions imported to routes(1 issue to fix)

--- 10-personnelAPI-2/src/routes/personnel.js
+++ 10-personnelAPI-2/src/routes/personnel.js
@@ -3,6 +3,7 @@
 const router = require('express').Router();
 
 const personnel = require('../controllers/personnel');
+const permissions = require('../middlewares/permissions');
 
 // URL: /personnels
 
@@ -10,13 +11,13 @@ router.post('/login', personnel.login);
 router.all('/logout', personnel.logout);
 
 router.route('/')
-    .get(personnel.list)
-    .post(personnel.create)
+    .get(permissions.isAdmin, personnel.list)
+    .post(permissions.isAdmin, personnel.create)
 
 router.route('/:id')
-    .get(personnel.read)
-    .put(personnel.update)
-    .patch(personnel.update)
-    .delete(personnel.delete)
+    .get(permissions.isAdminOrOwn, personnel.read)
+    .put(permissions.isAdminOrOwn, personnel.update)
+    .patch(permissions.isAdminOrOwn, personnel.update)
+    .delete(permissions.isAdmin, personnel.delete)
 
 module.exports = router;
