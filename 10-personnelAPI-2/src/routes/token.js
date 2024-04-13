cd975813c7095ed4e34291da869a91ae1a418d34
Betul <betulkoru06@gmail.com>
2024-04-13 22:24:45 +0300

    token router

--- 10-personnelAPI-2/src/routes/token.js
+++ 10-personnelAPI-2/src/routes/token.js
@@ -4,5 +4,20 @@ const router = require('express').Router();
 
 const token = require('../controllers/token');
 
+const { isAdmin } = require('../middlewares/permissions');
+
 // URL: /tokens
 
+router.use(isAdmin)
+
+router.route('/')
+    .get(token.list)
+    .post(token.create)
+
+router.route('/:id')
+    .get(token.read)
+    .put(token.update)
+    .patch(token.update)
+    .delete(token.delete)
+
+module.exports = router;
