4f0a57b9de856962992d0d996bc7be74cc3c86a2
Betul <betulkoru06@gmail.com>
2024-04-14 23:38:42 +0300

    permissions

---
+++ 11-pizzaAPI-2/src/middlewares/permissions.js
@@ -0,0 +1,14 @@
+'use strict'
+
+module.exports = {
+
+    isLogin: (req, res, next) => {
+
+        if (req.user && req.user.isActive) {
+            next();
+        } else {
+            res.errorStatusCode = 403;
+            throw new Error('NoPermission: You must login.');
+        }
+    },
+}
