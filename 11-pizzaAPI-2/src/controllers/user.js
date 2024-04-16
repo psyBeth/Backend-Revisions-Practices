3801973c25de998a43851a967306ce5dc6bdc2c8
Betul <betulkoru06@gmail.com>
2024-04-16 22:28:58 +0300

    user controller struct

--- 11-pizzaAPI-2/src/controllers/user.js
+++ 11-pizzaAPI-2/src/controllers/user.js
@@ -1,3 +1,29 @@
 'use strict'
 
-//TODO: user controller will be built first
+const User = require('../models/user');
+const Token = require('../models/token');
+const passwordEncrypt = require('../helpers/passwordEncrypt');
+
+module.exports = {
+
+    list: async(req, res) => {
+
+    },
+
+    create: async(req, res) => {
+
+    },
+
+    read: async(req, res) => {
+
+    },
+
+    update: async(req, res) => {
+
+    },
+
+    delete: async(req, res) => {
+
+    }
+
+};
