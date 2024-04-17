4ba529cea2780632cc3833e0b2ae116dd259ce9c
Betul <betulkoru06@gmail.com>
2024-04-17 16:52:14 +0300

    user model completed

--- 11-pizzaAPI-2/src/models/user.js
+++ 11-pizzaAPI-2/src/models/user.js
@@ -18,18 +18,34 @@ const UserSchema = new mongoose.Schema({
         type: String,
         trim: true,
         required: true,
+        set: (password) => {
+            if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password)) {
+                return passwordEncrypt(password);
+            } else {
+                throw new Error('Password type is not correct.');
+            };
+        }
     },
 
     email: {
-
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        validate: [
+            (email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email),
+            'Email type is not correct.'
+        ]
     },
 
     isActive: {
-
+        type: Boolean,
+        default: true
     },
 
     isAdmin: {
-
+        type: Boolean,
+        default: false
     }
 
 } , {
