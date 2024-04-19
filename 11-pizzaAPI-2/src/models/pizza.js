6b49d33a0597a4f80646c6adf9ae91022025223e
Betul <betulkoru06@gmail.com>
2024-04-19 22:17:49 +0300

    pizza & topping model

--- 11-pizzaAPI-2/src/models/pizza.js
+++ 11-pizzaAPI-2/src/models/pizza.js
@@ -1,1 +1,33 @@
 'use strict'
+
+const { mongoose } = require('../configs/dbConnection');
+
+const PizzaSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+    },
+    
+    image: String,
+
+    price: {
+        type: Number,
+        required: true,
+    },
+
+    toppingIds: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Topping',
+        }
+    ]
+
+}, {
+    collection: 'pizzas',
+    timestamps: true
+});
+
+module.exports = mongoose.model('Pizza', PizzaSchema);
